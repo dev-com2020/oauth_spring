f3cc0387e05953b5df27035ebe1273183b835f58
dev-com2020 <kwp@tlen.pl>
2023-04-28 10:44:27 +0200

    Initial commit

---
+++ oauth/src/main/java/com/example/oauth/YouTubeConfig.java
@@ -0,0 +1,24 @@
+package com.example.oauth;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class YouTubeConfig {
+    static String YOUTUBE_V3_API = "https://www.googleapis.com/youtube/v3";
+
+    @Bean
+    WebClient webClient(OAuth2AuthorizedClientManager clientManager) {
+ServletOAuth2AuthorizedClientExchangeFilterFunction
+    ouath2 = new ServletOAuth2AuthorizedClientExchangeFilterFunction(clientManager);
+    ouath2.setDefaultClientRegistrationId("google");
+
+        return WebClient.builder()
+                .baseUrl(YOUTUBE_V3_API)
+                .apply(ouath2.oauth2Configuration())
+                .build();
+    }
+}
